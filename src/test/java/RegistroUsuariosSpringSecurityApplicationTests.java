846ee00776e77f73e48f13b5203602b207c6715a
sstrafe <135684754+sstrafe@users.noreply.github.com>
2025-07-03 13:23:56 -0500

    fix: especificar clase principal en @SpringBootTest

--- src/test/java/RegistroUsuariosSpringSecurityApplicationTests.java
+++ src/test/java/RegistroUsuariosSpringSecurityApplicationTests.java
@@ -1,7 +1,8 @@
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.cibertec.app.SpringBootSesion07Application;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringBootSesion07Application.class)
 class RegistroUsuariosSpringSecurityApplicationTests {
 
 	@Test
